f3b18d68e7576ebf62575898dd5702a9f6157d5c
Riya <21cs74@ecajmer.ac.in>
2023-12-05 20:02:14 +0530

    2nd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
